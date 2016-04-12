90c2e66dbfd1fdf8364770080532b48e75c8f8cf
elgatoluchin@gmail.com <elgatoluchin@gmail.com>
2016-04-12 10:42:39 -0300

    POO Herencia y encapsulamiento en C#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class LibroCalificaciones{
+    //Variable de instancia de tipo privado
+    private string nombreCurso;
+
+    
+    //Porpiedad para obtener (get) y establecer (set) el nombre del Curso
+    public string NombreCurso{
+        get{ 
+            return nombreCurso;
+            }
+        set{
+            nombreCurso = value;
+        }
+    }//Fin metodo
+
+    //Metodo de retorno
+    public void MostrarMensaje(){
+        //Mostramos la Propiedad NombreCurso
+        Console.WriteLine("Bienvenido al libro de calificaciones para: {0}",NombreCurso);
+    }//Fin del metodo
+}//Fin de la clase
---
+++ cls_PruebaLibroCalificaciones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class cls_PruebaLibroCalificaciones{
+
+    public static void Main(String [] args){
+
+        //el argumento CS101 Introduccion a la programacion en C#
+        //Se pasa al constructor del objeto y se utiliza para inicializar nombreCurso
+        //Esta instruccion requiere que la clase proporcione un contructor con un parametro string
+        LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones("CS101 Introduccion a la programacion en C#");
+
+        Console.WriteLine("El nombre del curso es: {0}", miLibroCalificaciones.NombreCurso);
+
+        Console.WriteLine("Escriba el nombre del curso: ");
+        string elNombre = Console.ReadLine();
+
+        //el objeto de tipo miLibroCalificaciones
+        //Hace referencia a la propiedad de objeto LibroCalificaciones
+        //y envia el valor a la porpiedad NombreCurso 
+        miLibroCalificaciones.NombreCurso = elNombre;
+
+        //Mostramos los valores del metodo MostrarMensaje
+        //del objeto miLibroCalificaiones que representa 
+        // al objeto LibroCalificaciones que esta en memoria 
+        miLibroCalificaciones.MostrarMensaje();
+
+
+    }//Fin del metodo
+
+}//Fin de la Clase
